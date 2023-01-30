73f5995a93ef62163692428be729493d99758e2e
Ranjithkumar1510 <ranjithkumar.b.1511@gmail.com>
2023-01-30 14:32:21 +0530

    added MOC and Rankine

---
+++ Python_codes/01_Rankine_Oval/Documentations/Rankine_oval/Code/Rankine_Oval.py
@@ -0,0 +1,100 @@
+#!/bin/python3
+
+# Rankine -fULL Oval
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Quadrilateral mesh grid
+x,y=np.meshgrid(np.linspace(0,1,200),np.linspace(0,1,200))
+
+# Number of grid ponits in x axis
+Nx=len(x)
+
+# Number of grid ponits in y axis
+Ny=len(y)
+
+# x directional velocity commponenit array
+u=np.zeros([Nx,Ny])
+
+# y directional velocity component array
+v=np.zeros([Nx,Ny])
+
+# Free stream velocity
+V_inf=10
+
+# Source and sink strength
+Lamda=100
+
+# x location of the source
+xc1=0.48
+
+# x location of the sink
+xc2=0.52
+
+# The distace between the source/sink to the center
+b=(xc2-xc1)/2
+
+# location of the center
+xc=0.5
+yc=0.5
+
+# computing section
+# polar coordinate values from the center
+
+# radius
+r=np.sqrt((x-xc)**2+(y-yc)**2)
+# Angle of the each postion w.r.t center
+theta=np.arctan2(y-yc,x-xc)
+# Angle of the each postion w.r.t source center
+t1=np.arctan2(y-yc,x-xc1)
+# Angle of the each postion w.r.t sink center
+t2=np.arctan2(y-yc,x-xc2)
+
+# Calculating Stream function 
+psi=V_inf*r*np.sin(theta)+(Lamda*(t1-t2)/2/np.pi)
+
+
+# Numerical computing section
+# Calculating velocity components 
+for i in range(0,Nx-1):
+    for j in range(0,Ny-1):
+        u[j,i]=(psi[j+1,i]-psi[j,i])/(y[j+1,i]-y[j,i])
+        v[j,i]=-(psi[j,i+1]-psi[j,i])/(x[j,i+1]-x[j,i])
+
+# calculating last grid ponit velocity components
+for i in range(0,Nx):
+    u[i,Nx-1]=2*u[i,Nx-2]-u[i,Nx-3]
+    u[Nx-1,i]=2*u[Nx-2,i]-u[Nx-3,i]
+    
+    v[i,Nx-1]=2*v[i,Nx-2]-v[i,Nx-3]
+    v[Nx-1,i]=2*v[Nx-2,i]-v[Nx-3,i]
+    
+# Analytical solution 
+
+# Offceting the Coordination
+X=x-xc
+Y=y-yc
+
+# x component velocity
+a1=(X+b)/((X+b)**2+Y**2)
+a2=(X-b)/((X-b)**2+Y**2)
+ana_u=V_inf+(Lamda*(a1-a2)/2/np.pi)
+
+# y component velocity
+a1=Y/((X+b)**2+Y**2)
+a2=Y/((X-b)**2+Y**2)
+ana_v=Lamda*(a1-a2)/2/np.pi
+
+# plotting section
+
+plt.figure()
+# plotting the numerical solution
+plt.streamplot(x,y,u,v)
+# plotting the analytical solution
+plt.streamplot(x,y,ana_u,ana_v)
+plt.title("Rankine Oval")
+plt.savefig("Result.png",dpi=150)
+plt.show()
+
+# Maximum 
---
+++ Python_codes/02_MOC/01_Compresion_Corner/compresion_corner.py
@@ -0,0 +1,200 @@
+#!/bin/python3
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+#  --------------------------------------------------------------------------------------
+# initial conditions
+
+# specific heat constant of air
+g=1.4
+
+# inlet Mach number
+Mi= 3.5 
+
+# Inlet Node points
+Ni= 10 
+
+# Total wall points
+Nw= 5
+
+# Theta values for Bottom , top and inlet Nodes
+bottom_t=np.radians(np.linspace(-6,0,Nw))
+top_t=np.radians(0)
+inlet_t=np.radians(np.linspace(-6,0,Ni))
+
+# Height of the inlet
+H=1
+
+#  --------------------------------------------------------------------------------------
+# Prandtl meyer function
+
+def fun_Nu(M):
+    a=(g+1)/(g-1)
+    b=M**2-1
+    nu=np.sqrt(a)*np.arctan(np.sqrt(b/a))-np.arctan(np.sqrt(b))
+
+    return nu
+
+#  --------------------------------------------------------------------------------------
+# Mach number getting from P-M function
+    # using Bi-section Method
+
+def fun_M(nu):
+    a=0.1
+    b=10
+
+    while True:
+        c=(a+b)/2
+        res=fun_Nu(c)-nu
+        if res < 0:
+            a=c
+        else:
+            b=c
+
+        c=(a+b)/2
+        res=fun_Nu(c)-nu
+        # print(c,res)
+        if np.abs(res)<1e-6:
+            break
+
+    return c
+
+#  --------------------------------------------------------------------------------------
+# Total number of points
+Nt=(Ni*Nw)+((Ni-1)*(Nw-1))
+
+# Needed array for Node
+Node=np.zeros(Nt,dtype=int)
+btm=np.zeros(Nw,dtype=int)
+top=np.zeros(Nw,dtype=int)
+inlet=np.zeros(Ni,dtype=int)
+internal=np.zeros(((Nt-1)-(2*Nw)),dtype=int)
+
+# Total Node
+for i in range(Nt):
+    Node[i]=i
+
+# Inlet Node
+for i in range(Ni):
+    inlet[i]=i
+
+# Top and Bottom Node
+for i in range(Nw):
+    btm[i]=i*(2*Ni-1)
+    top[i]=btm[i]+(Ni-1)
+
+# internal Nodes
+count=0
+for i in range(Nt):
+    if i in inlet or i in btm or i in top:
+        continue
+    else:
+        internal[count]=i
+        count=count+1
+
+#  --------------------------------------------------------------------------------------
+# necessary Arrays
+M=np.zeros(Nt)
+K1=np.zeros(Nt)
+K2=np.zeros(Nt)
+nu=np.zeros(Nt)
+theta=np.zeros(Nt)
+Mu=np.zeros(Nt)
+
+count=1
+# Computing Mach number
+for i in range(Nt):
+    if i in inlet:
+        M[i]=Mi
+        theta[i]=inlet_t[i]
+        nu[i]=fun_Nu(M[i])
+        K1[i]=nu[i]+theta[i]
+        K2[i]=nu[i]-theta[i]
+        Mu[i]=np.arcsin(1/M[i])
+    elif i in btm:
+        theta[i]=bottom_t[count]
+        K1[i]=K1[i-(Ni-1)]
+        K2[i]=K1[i]-(2*theta[i])
+        nu[i]=(K1[i-(Ni-1)]+K2[i])/2
+        M[i]=fun_M(nu[i])
+        Mu[i]=np.arcsin(1/M[i])
+        count=count+1
+    elif i in top:
+        theta[i]=top_t
+        K2[i]=K2[i-Ni]
+        K1[i]=(2*theta[i])+K2[i]
+        nu[i]=(K1[i]+K2[i-Ni])/2
+        M[i]=fun_M(nu[i])
+        Mu[i]=np.arcsin(1/M[i])
+    else:
+        theta[i]=(K1[i-(Ni-1)]-K2[i-Ni])/2
+        nu[i]=(K1[i-(Ni-1)]+K2[i-Ni])/2
+        K1[i]=nu[i]+theta[i]
+        K2[i]=nu[i]-theta[i]
+        M[i]=fun_M(nu[i])
+        Mu[i]=np.arcsin(1/M[i])
+
+#  --------------------------------------------------------------------------------------
+# Computing the location
+x=np.zeros(Nt)
+y=np.zeros(Nt)
+
+# height between the two consicutive points in the inlet
+h = H/(Ni-1)
+for i in range(Nt):
+    if i in inlet:
+        x[i]=0
+        y[i]=i*h
+    elif i in btm:
+        # only right running curve present
+        S1=np.tan(theta[i-(Ni-1)]-Mu[i-(Ni-1)])
+        x[i]=(y[i-(Ni-1)]-x[i-(Ni-1)]*S1)/(np.tan(theta[i])-S1)
+        y[i]=x[i]*np.tan(theta[i])
+    elif i in top:
+        S2=np.tan(theta[i]+Mu[i])
+        x[i]=(y[i-Ni]-x[i-Ni]*S2-H)/(np.tan(theta[i])-S2)
+        y[i]=(x[i]*np.tan(theta[i]))+H
+    else:
+        S1=(np.tan(theta[i]+Mu[i])+np.tan(theta[i-Ni]+Mu[i-Ni]))/2
+        S2=(np.tan(theta[i]-Mu[i])+np.tan(theta[i-(Ni-1)]-Mu[i-(Ni-1)]))/2
+        x[i]=((S2*x[i-(Ni-1)]-S1*x[i-Ni])+(y[i-Ni]-y[i-(Ni-1)]))/(S2-S1)
+        y[i]=y[i-(Ni)]+((x[i]-x[i-(Ni)])*S1)
+#  --------------------------------------------------------------------------------------
+# DataFrame
+
+df=pd.DataFrame(np.transpose([M,K1,K2,theta,nu,Mu,x,y]),columns=["M","K1","K2","Theta","Nu","Mu","X","Y"])
+print(df)
+
+#  --------------------------------------------------------------------------------------
+# plotting section
+Yt=np.zeros(Nw)
+Xt=np.zeros(Nw)
+Xb=np.zeros(Nw) ; Yb=np.zeros(Nw)
+for i in range(Nw):
+    Xt[i]=x[top[i]]
+    Yt[i]=y[top[i]]
+
+for i in range(Nw):
+    Xb[i]=x[btm[i]]
+    Yb[i]=y[btm[i]]
+
+plt.figure()
+plt.plot(df["X"],df["Y"],'ko')
+plt.plot(Xt,Yt,'-k',linewidth=2)
+plt.plot(Xb,Yb,'-k',linewidth=2)
+plt.axis("image")
+plt.savefig("Grid.png",dpi=150)
+plt.show()
+
+plt.figure()
+plt.tricontourf(df["X"],df["Y"],df["M"],100,cmap="jet")
+plt.plot(Xt,Yt,'-k',linewidth=2)
+plt.plot(Xb,Yb,'-k',linewidth=2)
+plt.axis("image")
+plt.colorbar(location='top')
+plt.savefig("Contour.png",dpi=150)
+plt.show()
+
+
